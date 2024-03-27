4e4e15263865e020d38d00e8251b8bb0bb0bb6e6
ErrNotNill <andreevivan8891@gmail.com>
2024-03-27 22:27:00 +0300

    some fixes

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -1,10 +1,16 @@
 package routes
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func Router() {
 
-	/*http.HandleFunc("/api/auth", authorization.GetAuthCode)
+	http.HandleFunc("/api/test", TestHandler)
 
-	c := cors.New(cors.Options{
+	/*c := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"}, // Change this to the specific origin of your Vue.js app in a production environment.
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{"Authorization", "Content-Type"},
@@ -14,4 +20,14 @@ func Router() {
 	http.Handle("/api/login_page", c.Handler(http.HandlerFunc(repository.LoginPage)))
 	http.HandleFunc("/api/redirect", repository.RedirectPage) //here user redirects from login page*/
 
+}
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(fmt.Sprintf("OK")))
+	if err != nil {
+		log.Println("Error redirect", err.Error())
+		return
+	}
+	fmt.Println(r.Body)
 }
--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	fmt.Println("service starting...")
+
 	if err := godotenv.Load(filepath.Join(".env")); err != nil {
 		log.Print("No .env file found")
 	} else {
