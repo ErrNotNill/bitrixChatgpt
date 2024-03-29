7b73c0998b5b53f62d84b6f2d8cd9a3723a4f3e8
ErrNotNill <andreevivan8891@gmail.com>
2024-03-29 22:19:04 +0300

    some fixes

--- backend/bitrix/authorize/authorize.go
+++ backend/bitrix/authorize/authorize.go
@@ -2,6 +2,7 @@ package authorize
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -26,6 +27,105 @@ func ConnectionBitrix(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Println("resp_at_first:", string(bs))
 	defer r.Body.Close()
+
+	ts, err := template.ParseFiles("backend/bitrix/authorize/index.html")
+	if err != nil {
+		log.Println("error parsing")
+	}
+	err = ts.Execute(w, r)
+	if err != nil {
+		log.Println("error executing")
+	}
+
+	/*values, err := url.ParseQuery(string(bs))
+	if err != nil {
+		log.Println("error parsing query:", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+	}
+
+	authExpires, err := strconv.Atoi(values.Get("AUTH_EXPIRES"))
+	if err != nil {
+		log.Println("error converting AUTH_EXPIRES to int:", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+	}
+
+	auth := AuthRequest{
+		AuthID:           values.Get("AUTH_ID"),
+		AuthExpires:      authExpires,
+		RefreshID:        values.Get("REFRESH_ID"),
+		MemberID:         values.Get("member_id"),
+		Status:           values.Get("status"),
+		Placement:        values.Get("PLACEMENT"),
+		PlacementOptions: values.Get("PLACEMENT_OPTIONS"),
+	}
+
+	fmt.Println("auth.AuthId:", auth.AuthID)
+	fmt.Println("auth.RefreshId:", auth.RefreshID)
+
+	clientId := os.Getenv("BITRIX_CLIENT_ID")
+	method := "POST"
+
+	requestUrl := fmt.Sprintf("https://b24-9f7fvg.bitrix24.ru/oauth/authorize/?client_id=%s", clientId)
+
+	req, err := http.NewRequest(method, requestUrl, nil)
+	if err != nil {
+		log.Println("error creating new request:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	//req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("error sending request:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	bz, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("error reading response body:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	log.Println("resp_at_last:", string(bz))
+	if err := GetDeals(auth.AuthID); err != nil {
+		// Handle error if adding a deal fails
+		http.Error(w, "Failed to get deal", http.StatusInternalServerError)
+		return
+	}
+	//http.Redirect(w, r, "https://b24app.rwp2.com/", http.StatusFound)
+	if err := AddDeal(auth.AuthID); err != nil {
+		// Handle error if adding a deal fails
+		http.Error(w, "Failed to add deal", http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse, err := json.Marshal(auth)
+	if err != nil {
+		log.Println("error marshalling AuthRequest to JSON:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON response
+	_, err = w.Write(jsonResponse)
+	if err != nil {
+		log.Println("error writing JSON response:", err)
+		// You might choose not to send another HTTP error here if the header has already been written
+		return
+	}
+
+
+	redirectURL := "https://b24app.rwp2.com/"
+
+	// Use http.Redirect to redirect the client
+	// The http.StatusFound status code is commonly used for redirects
+	http.Redirect(w, r, redirectURL, http.StatusFound)*/
 
 }
 
