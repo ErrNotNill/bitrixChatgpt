7442e38d7bb8d336aa3d1fca631a9d3f2a5a1327
ErrNotNill <andreevivan8891@gmail.com>
2024-03-30 13:39:22 +0300

    some fixes

--- backend/bitrix/service/events/events.go
+++ backend/bitrix/service/events/events.go
@@ -1,10 +1,12 @@
 package events
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func OnCrmDealAddEvent(w http.ResponseWriter, r *http.Request) {
@@ -16,6 +18,40 @@ func OnCrmDealAddEvent(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Println("resp_at_first:", string(bs))
 	defer r.Body.Close()
+
+	values, err := url.ParseQuery(string(bs))
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert to a map (manually handling nested structures)
+	result := make(map[string]interface{})
+	for key, value := range values {
+		// Assuming only one value per key for simplicity
+		result[key] = value[0]
+	}
+
+	// Converting the map to JSON
+	jsonBytes, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(jsonBytes))
+
+	// To extract the ID, you can unmarshal the relevant part of the JSON back into a Go data structure
+	// or manipulate the JSON directly if you prefer
+	type DataFields struct {
+		ID string `json:"data[FIELDS][ID]"`
+	}
+
+	var dataFields DataFields
+	if err := json.Unmarshal(jsonBytes, &dataFields); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Extracted ID:", dataFields.ID)
+
 }
 
 func OnCrmDealAddEventRegistration(authId string) {
