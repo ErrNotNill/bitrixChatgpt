40482879f4d538aa1a29d94d36ae177f26b2e0da
ErrNotNill <andreevivan8891@gmail.com>
2024-03-30 20:35:53 +0300

    some fixes

--- backend/bitrix/service/transfer.go
+++ backend/bitrix/service/transfer.go
@@ -9,22 +9,29 @@ import (
 
 func TransferDealsOnVue(w http.ResponseWriter, r *http.Request) {
 
-	deals, err := GetDeals(authorize.GlobalAuthId)
-	if err != nil {
-		log.Println("error getting service: ", err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	if r.Method == "GET" {
+		log.Println("TransferDealsOnVue method is GET")
+		deals, err := GetDeals(authorize.GlobalAuthId)
+		if err != nil {
+			log.Println("error getting service: ", err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		// Set content type to application/json
+		w.Header().Set("Content-Type", "application/json")
+
+		// Log the deals for debugging
+		fmt.Println("TransferDealsOnVue deals: ", string(deals))
+
+		// Write the JSON data to the response
+		_, writeErr := w.Write(deals)
+		if writeErr != nil {
+			log.Println("error writing service to response: ", writeErr.Error())
+		}
+
+	} else {
+		log.Println("something wrong...")
 	}
 
-	// Set content type to application/json
-	w.Header().Set("Content-Type", "application/json")
-
-	// Log the deals for debugging
-	fmt.Println("TransferDealsOnVue deals: ", string(deals))
-
-	// Write the JSON data to the response
-	_, writeErr := w.Write(deals)
-	if writeErr != nil {
-		log.Println("error writing service to response: ", writeErr.Error())
-	}
 }
