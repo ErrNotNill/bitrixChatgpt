8858126d8b4e589e3800b91eb5d430c1dfde15d1
ErrNotNill <andreevivan8891@gmail.com>
2024-03-30 14:51:22 +0300

    some fixes

---
+++ backend/bitrix/service/events/event_registration.go
@@ -0,0 +1,38 @@
+package events
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func OnCrmDealAddEventRegistration(authId string) {
+
+	method := "POST"
+	event := "OnCrmDealAdd"
+	handler := "https://b24app.rwp2.com/api/event_deal_add"
+
+	// Format the URL with the provided authID parameter
+	requestUrl := fmt.Sprintf("https://b24-9f7fvg.bitrix24.ru/rest/event.bind.json?auth=%s&auth_type=0&event=%s&event_type=offline&handler=%s", authId, event, handler)
+
+	req, err := http.NewRequest(method, requestUrl, nil)
+	if err != nil {
+		log.Println("error creating new request:", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("error sending request:", err)
+	}
+	defer resp.Body.Close()
+
+	bz, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("error reading response body:", err)
+	}
+	log.Println("resp_at_last_AddDeal:", string(bz))
+
+}
